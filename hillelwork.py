e0a9e1d2c819925b9850a98e4a0524229c98cf10
Сергей Гладченко <serega.gladch1155@gmail.com>
2021-10-19 00:39:14 +0300

    initial commit

---
+++ hillelwork.py
@@ -0,0 +1,20 @@
+user = input("Введите целое число:")
+user = int(user)
+
+result = user * 2
+print(result)
+
+###############################
+
+user = float(input("Введите дробное число:"))
+
+result = user * 2
+print(result)
+
+###############################
+
+user = input("Введите любую строку:")
+user = str(user)
+
+result = user * 2
+print(result)
