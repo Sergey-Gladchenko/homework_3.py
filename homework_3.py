d2b708083160ab19869e8583995d58ca7a299ee2
Сергей Гладченко <serega.gladch1155@gmail.com>
2021-10-29 21:24:52 +0300

    3 homework

--- homework_3.py
+++ homework_3.py
@@ -1,17 +1,18 @@
-value = 211
-my_value = int(value / 2) if value < 100 else int(value / (-1))
+value = 21
+my_value = value / 2 if value < 100 else value * (-1)
 print(my_value)
 
 #####################################################
 
 value = 19
-my_value = int(value / value) if value < 100 else int(value * 0)
+my_value = 1 if value < 100 else 0
 print(my_value)
 
 #####################################################
 
-value = 11
-my_value = print(True) if value < 100 else print(False)
+value = 110
+my_value = True if value < 100 else False
+print(my_value)
 
 #####################################################
 
@@ -29,12 +30,12 @@ print(my_value)
 
 my_str = "karser"
 len_my_str = len(my_str)
-my_value = str(my_str * 2) if len_my_str < 5 else str(my_str)
+my_value = my_str * 2 if len_my_str < 5 else my_str
 print(my_value)
 
 #####################################################
 
 my_str = "kar"
 len_my_str = len(my_str)
-my_value = str(my_str + my_str[::-1]) if len_my_str <5 else str(my_str)
+my_value = my_str + my_str[::-1] if len_my_str <5 else my_str
 print(my_value)
